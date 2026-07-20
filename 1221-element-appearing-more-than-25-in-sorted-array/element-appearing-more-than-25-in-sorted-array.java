46070a0716b9d6a2e2c5b9dc4c8c96d6e0024b99
AMIT DUTTA <amit8391810917@gmail.com>
2026-07-20 14:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (81.23%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,17 +1,19 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
         int n = arr.length;
-        Map<Integer,Integer> mpp = new HashMap<>();
-        for(int ele : arr){
-            mpp.put(ele,mpp.getOrDefault(ele,0)+1);
-        }
-        int ans = 0;
-        for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
-            Integer value = entry.getValue();
-            if(value>n/4) {
-                ans = entry.getKey();
+        int cnt = 1;
+        int presentEle = arr[0];
+        for(int i = 1 ; i < n ; i++){
+            if(arr[i] == presentEle){
+                cnt++;
+                if(4*cnt>n)
+                    return presentEle;
+            }
+            else{
+                presentEle = arr[i];
+                cnt = 1;
             }
         }
-        return ans;
+        return presentEle;
     }
 }
